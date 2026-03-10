f51575211f2af4e45988b6839f6ee216aa3c6797
오준혁 <74858961+JHCard7872@users.noreply.github.com>
2026-03-10 10:03:26 +0900

    Scene base

--- Jungle_Game/Scene.h
+++ Jungle_Game/Scene.h
@@ -1,1 +1,10 @@
 #pragma once
+
+class Scene {
+public:
+    virtual ~Scene() {}
+    virtual void Initialize() = 0;
+    virtual void Update(float dt) = 0;
+    virtual void Render() = 0;
+    virtual void Release() = 0;
+};
