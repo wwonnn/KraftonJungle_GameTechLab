38790879e764c02e6e471344af3a3981e0ab13e7
kwonhyeonsoo-goo <kkhhss4035@gmail.com>
2026-03-24 14:18:27 +0900

    edit. billboard SubUV

--- World/Primitives/SubUV/SubUV.h
+++ World/Primitives/SubUV/SubUV.h
@@ -5,6 +5,19 @@
 class USubUVComponent :
     public UPrimitiveComponent
 {
+public:
+    USubUVComponent(std::wstring filename = L"Assets/Effects/Explosion.PNG");
+    bool GetRenderCommand(const FMatrix& viewMatrix, const FMatrix& projMatrix, FRenderCommand& OutCommand) override;
+    void Update(float deltatime) override;
+    EPrimitiveType GetPrimitiveType() const override { return PrimitiveType; }
+
+public:
+    void UpdateWorldAABB() override;
+
+    bool bIsBillBoard = true;
+    float Timer;
+    static constexpr EPrimitiveType PrimitiveType = EPrimitiveType::EPT_SubUV;
+
 private:
     const FUVMeshData* MeshData = nullptr;
     int Texture2DId = -1;
@@ -16,22 +29,12 @@ private:
     std::wstring filename;
     bool bLoop = true;
     float PlayRate = 24.f;
-    bool bIsBillBoard = true;
-    float currentU, currentV;
-public:
-    void UpdateFrame(float deltaTime);
-    USubUVComponent(std::wstring filename = L"Assets/Effects/Explosion.PNG");
-    bool GetRenderCommand(const FMatrix& viewMatrix, const FMatrix& projMatrix, FRenderCommand& OutCommand) override;
-    void Update(float deltatime) override;
 
-    EPrimitiveType GetPrimitiveType() const override { return PrimitiveType; }
+    float currentU, currentV;
+    float ElaspedTime;
 
 private:
     void ReloadTextureResource(std::wstring filename = L"Assets/Effects/Explosion.PNG");
+    void UpdateFrame(float deltaTime);
 
-public:
-    void UpdateWorldAABB() override;
-    float ElaspedTime;
-    float Timer;
-    static constexpr EPrimitiveType PrimitiveType = EPrimitiveType::EPT_SubUV;
 };
