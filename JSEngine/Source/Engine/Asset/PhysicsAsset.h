2220d7d58f176fd6b39586e6ef4e4f9c695c4514
JunHyeop3631 <sstechangle@gmail.com>
2026-05-17 17:51:35 +0900

    feat: Add Skeleton and Physics asset classes
    Introduce skeleton and physics asset types and integrate them into the project. Adds FSkeletonData/FSkeletonBone/FSkeletonSocket/FSkeletonCurveMetaData and USkeleton (Skeleton.h/.cpp/.h types) for skeleton representation and lookup utilities. Adds FPhysicsAssetData/FPhysicsBody/FPhysicsConstraint and UPhysicsAsset (PhysicsAsset.h/.cpp) providing storage, accessors, and validity checks for physics bodies and constraints. Updates JSEngine.vcxproj and JSEngine.vcxproj.filters to include the new source and header files.

---
+++ JSEngine/Source/Engine/Asset/PhysicsAsset.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "Core/CoreMinimal.h"
+#include "Engine/Geometry/Transform.h"
+#include "Object/FName.h"
+#include "Object/Object.h"
+
+class USkeleton;
+
+enum class EPhysicsBodyShape : uint8
+{
+    Box = 0,
+    Sphere,
+    Capsule
+};
+
+struct FPhysicsBody
+{
+    FName Name;
+    int32 BoneIndex = -1;
+
+    EPhysicsBodyShape Shape = EPhysicsBodyShape::Capsule;
+    FTransform LocalTransform = FTransform::Identity;
+
+    FVector BoxExtent = FVector::ZeroVector;
+    float SphereRadius = 0.0f;
+    float CapsuleRadius = 0.0f;
+    float CapsuleHalfHeight = 0.0f;
+};
+
+struct FPhysicsConstraint
+{
+    FName Name;
+    int32 ParentBodyIndex = -1;
+    int32 ChildBodyIndex = -1;
+
+    FTransform ParentLocalFrame = FTransform::Identity;
+    FTransform ChildLocalFrame = FTransform::Identity;
+};
+
+struct FPhysicsAssetData
+{
+    FString PathFileName;
+    FString SkeletonAssetPath;
+
+    TArray<FPhysicsBody> Bodies;
+    TArray<FPhysicsConstraint> Constraints;
+
+    void Clear();
+    bool HasValidBodyData() const;
+};
+
+class UPhysicsAsset : public UObject
+{
+public:
+    DECLARE_CLASS(UPhysicsAsset, UObject)
+
+    UPhysicsAsset() = default;
+    ~UPhysicsAsset() override;
+
+    void SetPhysicsAssetData(FPhysicsAssetData* InPhysicsAssetData);
+
+    FPhysicsAssetData* GetPhysicsAssetData();
+    const FPhysicsAssetData* GetPhysicsAssetData() const;
+
+    void SetSkeleton(USkeleton* InSkeleton);
+    USkeleton* GetSkeleton() const;
+
+    const FString& GetAssetPathFileName() const;
+    const FString& GetSkeletonAssetPath() const;
+
+    const TArray<FPhysicsBody>& GetBodies() const;
+    const TArray<FPhysicsConstraint>& GetConstraints() const;
+
+    const FPhysicsBody* GetBody(int32 BodyIndex) const;
+    int32 FindBodyIndex(const FName& Name) const;
+    const FPhysicsBody* FindBody(const FName& Name) const;
+
+    bool HasValidPhysicsData() const;
+
+private:
+    FPhysicsAssetData* PhysicsAssetData = nullptr;
+    USkeleton* Skeleton = nullptr;
+};
